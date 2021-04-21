8b7e75ece3ae4057922e9248678e55317b50b901
gurudanito001 <youareontodan@gmail.com>
2021-04-21 10:43:23 +0100

    edited index and db files

--- index.js
+++ index.js
@@ -31,6 +31,19 @@ app.get("/users/:id", async (req, res) => {
     }
 });
 
+app.get('/db', async (req, res) => {
+    try {
+        const client = await pool.connect();
+        const result = await client.query('SELECT * FROM users');
+        const results = { 'results': (result) ? result.rows : null };
+        res.render('pages/db', results);
+        client.release();
+    } catch (err) {
+        console.error(err);
+        res.send("Error " + err);
+    }
+})
+
 
 app.listen( 5000, () => {
     console.log(`server is listening on port 5000`)
